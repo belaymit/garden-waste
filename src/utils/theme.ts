60af4ab9795105cefc916cae73a3a9fc59e57af2
belaymit <2belamit@gmail.com>
2025-06-07 22:12:01 +0300

    Added them configuration

---
+++ src/utils/theme.ts
@@ -0,0 +1,41 @@
+export default {
+  token: {
+    fontFamily: "InterTight",
+    colorPrimary: "#0086C9",
+    colorLink: "#FF4405",
+    colorText: "#475467",
+  },
+  components: {
+    Select: {
+      optionSelectedBg: "#FFF7E6",
+      colorPrimary: "#FF4405",
+    },
+    Steps: {
+      colorPrimary: "#FF4405",
+      finishIconBgColor: "#FFF7E6",
+      navArrowColor: "#0000000F",
+    },
+    Tabs: {
+      inkBarColor: "#FF4405",
+      itemSelectedColor: "#FF4405",
+    },
+    Segmented: {
+      itemSelectedBg: "#FF4405",
+      itemSelectedColor: "#FFF",
+      itemColor: "#000000E0",
+      trackBg: "#FFF",
+    },
+    Switch: {
+      colorPrimary: "#FF4405",
+    },
+    Slider: {
+      handleActiveColor: "#FF4405",
+      trackHoverBg: "#FF4405",
+      trackBg: "#E62E05",
+      dotActiveBorderColor: "#E62E05",
+    },
+    Progress: {
+      defaultColor: "linear-gradient(90deg, #FFB43B 0%, #FF5F29 100%)",
+    },
+  },
+};
